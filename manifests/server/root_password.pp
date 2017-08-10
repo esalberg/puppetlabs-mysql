a0461b904b33d55139aaffe95e809b8abc32ec1c
Elyse Salberg <elyse_salberg@putnam.com>
2017-08-10 11:17:04 -0400

    testing

--- manifests/server/root_password.pp
+++ manifests/server/root_password.pp
@@ -20,6 +20,7 @@ class mysql::server::root_password {
 
   # manage root password if it is set
   if $mysql::server::create_root_user == true and $mysql::server::root_password != 'UNSET' {
+    notify {'This is root@localhost time': }
     mysql_user { 'root@localhost':
       ensure        => present,
       password_hash => mysql_password($mysql::server::root_password),
@@ -28,6 +29,7 @@ class mysql::server::root_password {
   }
 
   if $mysql::server::create_root_my_cnf == true and $mysql::server::root_password != 'UNSET' {
+    notify {'This is .my.cnf time': }
     file { "${::root_home}/.my.cnf":
       content => template('mysql/my.cnf.pass.erb'),
       owner   => 'root',
